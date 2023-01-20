19061341f1f950baada1f863cb19ca67dea1d6c0
Jean Flores <jeancarlo_flores@hotmail.com>
2023-01-20 18:47:36 -0500

    ADD: github actions

--- src/app/controllers/home.controller.js
+++ src/app/controllers/home.controller.js
@@ -1,4 +1,7 @@
+const logProvider = require('../middlewares/logprovider');
+
 const home = async (req, res) => {
+    logProvider.info('Start home in home.controller.js')
     return res.send({message: "Hello Home"});
 }
 
